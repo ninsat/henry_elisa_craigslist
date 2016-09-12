726ea81e287e9a7ced3a77b1caf34571a26bf2a6
Elisa Verna & Henri El Daher <pair+elisuh.heldaher@devbootcamp.com>
2016-09-12 16:17:54 -0400

    added routes and views for articles and categories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 	def new
 	end
 
   def show
+  	@user = User.find(params[:id])
+  	@articles = @user.articles
   end
 
 	def create
+		user = User.new(user_params)
+		if user.save
+		  session[:user_id] = user.id 
+		  redirect_to categories_path
+		else
+			render new_session_path
+		end
 	end
 
 
